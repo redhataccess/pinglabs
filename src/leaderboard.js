c772682c3e0170bc6cdbd3f3bf55e343b962bced
Kyle Buchanan <kylebuch8@gmail.com>
2015-06-11 10:43:37 -0400

    updated scoring for leaderboard

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -1,11 +1,44 @@
 import { chunk, groupBy, first } from 'lodash';
 import * as conf from 'conf';
+import players from 'players';
 
 export let player_list       = {};
 export let player_list_parts = {};
 
 // const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 // const alpha_chunks = chunk(alphabet, conf.LOGIN_ALPHABET_CHUNK_SIZE);
+
+function score_handler(event) {
+    var player = event.detail.player,
+        score = event.detail.score,
+        xhr = new XMLHttpRequest(),
+        updatedPlayer = {};
+
+    if (score < player.ping.hiscore) {
+        return;
+    }
+
+    updatedPlayer[player.id] = {
+        ping: {
+            hiscore: score,
+            color: player.color
+        }
+    };
+
+    xhr.open('PUT', '/leaderboard/leaders.json');
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.responseType = 'json';
+
+    xhr.onload = function () {
+        console.log(xhr.response);
+    };
+
+    xhr.onerror = function () {
+        console.log('error');
+    };
+
+    xhr.send(JSON.stringify(updatedPlayer));
+}
 
 function get_player_list() {
     var request = new XMLHttpRequest();
@@ -26,3 +59,4 @@ function get_player_list() {
 }
 
 get_player_list();
+document.addEventListener('score', score_handler);
