d10296b13e39e1a5fde846848917770c0cd797b4
Kyle Buchanan <kylebuch8@gmail.com>
2015-06-15 17:04:08 -0400

    updated scoring timer

--- src/states/play.js
+++ src/states/play.js
@@ -163,18 +163,12 @@ function pause_click_handler() {
     }
 }
 
-function score_timer() {
-    let updateInterval = conf.SCORING_MS_PER_POINT;
-
-    function tick() {
-        for (var key in players) {
-            if (playing(players[key])) {
-                scores[key].add_score();
-            }
+function tick() {
+    for (var key in players) {
+        if (playing(players[key])) {
+            scores[key].add_score();
         }
     }
-
-    setInterval(tick, updateInterval);
 }
 
 export default class play_state extends state {
@@ -247,7 +241,7 @@ export default class play_state extends state {
         update_bg_color(game);
 
         // add points to players periodically
-        score_timer();
+        game.time.events.loop(Phaser.Timer.SECOND * 3, tick, this);
 
         let press_start_elements = document.querySelectorAll('.press-start');
         invoke(press_start_elements, 'addEventListener', 'click', press_start_click_handler, false);
