5a2de7fda23b8e2601b1fe837ad802d15f0a4b34
Kyle Buchanan <kylebuch8@gmail.com>
2015-06-16 10:45:07 -0400

    assign powerups to player after login

--- src/commands/player-login-choose-name.js
+++ src/commands/player-login-choose-name.js
@@ -2,6 +2,22 @@ import * as command from 'commands/command';
 import * as conf from 'conf';
 import * as leaderboard from 'leaderboard';
 import { findKey } from 'lodash';
+import players from 'players';
+import * as powerups from 'commands/powerups/all';
+
+let powerupHash = {
+    kickstart: 'kickstarter',
+    security: 'shellshock',
+    samba: 'puck_reverse'
+};
+
+function add_powerups(player, powerup, number) {
+    let i = 0;
+
+    for (i; i < number; i += 1) {
+        player.add_powerup(powerup);
+    }
+}
 
 export default class player_login_choose_name extends command {
 
@@ -24,6 +40,22 @@ export default class player_login_choose_name extends command {
     done() {
         console.log(`LOGIN: player ${this.player.name} is done choosing name`);
         this.player.id = findKey(leaderboard.player_list, leaderboard.selected_player);
+
+        /*
+         * assign powerups here. the player is assigned as many powerups
+         * as the games object specifies for each key
+         *
+         * kickstart = kick (kickstarter)
+         * security = hack (shellshock)
+         * samba = flip (puck_reverse)
+         */
+        if (leaderboard.selected_player.games) {
+            for (let key of Object.keys(leaderboard.selected_player.games)) {
+                let number = leaderboard.selected_player.games[key];
+                add_powerups(this.player, powerupHash[key], number);
+            }
+        }
+
         this.player.play.execute();
     }
 
