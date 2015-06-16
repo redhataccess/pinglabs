74be899328cf77d3645988d11d1d39db843f0542
mwcz <mwc@clayto.com>
2015-06-16 17:12:29 -0400

    Revert "remove massive duplicate in scorecards template"
    This reverts commit f966e3274e9bfbc72fb4a8a394c8548469d51dec.
    
    Conflicts:
    	src/scorecards.js

--- src/scorecards.js
+++ src/scorecards.js
@@ -3,18 +3,12 @@ import * as scorecard_template from 'text!templates/scorecards.html';
 import leaderboard from 'leaderboard';
 import * as conf from 'conf';
 import input from 'input';
-import { pick, each, assign, toArray, range, keys, get, sortBy as sort } from 'lodash';
+import { assign, toArray, range, keys, get, sortBy as sort } from 'lodash';
 import { inactive, playing, choosing_letter, choosing_name, logging_in } from 'player-state-checkers';
 
 let template = pc.template(scorecard_template, pc);
 let view;
 let data;
-
-// players-leaderboard-merge is a single object which combines the n,s,e,w
-// properties of players.js and leaderboard.js. paperclip seems to have trouble
-// with dirty checking separate objects within the same element/component, so
-// they are loosely combined here to make paperclipjs happy.
-let plm = {};
 
 let PlayerUI = pc.Component.extend({
     initialize: function () {
@@ -29,7 +23,8 @@ pc.components.playerui = PlayerUI;
 
 function create(players) {
     data = {
-        players: plm,
+        players: players,
+        leaderboard: leaderboard,
         conf,
         range,
         keys,
@@ -40,10 +35,7 @@ function create(players) {
     document.querySelector('#game-container').appendChild(view.render());
 }
 
-function update(players) {
-    each(['n', 's', 'e', 'w'], function (p) {
-        plm[p] = assign({}, players[p], leaderboard[p]);
-    });
+function update() {
     view.accessor.applyChanges();
 }
 
