c238b73c0d388ae5f858d8a04d316be11223caca
mwcz <mwc@clayto.com>
2015-06-18 15:12:41 -0400

    goals scored by FLIP are attributed to flipper :)

--- src/commands/powerups/puck-reverse.js
+++ src/commands/powerups/puck-reverse.js
@@ -1,6 +1,7 @@
 import Phaser from 'Phaser';
 import powerup from 'commands/powerup';
 import * as conf from 'conf';
+import * as puck_history from 'puck-history';
 
 class puck_reverse_powerup extends powerup {
     constructor(players, player) {
@@ -12,6 +13,10 @@ class puck_reverse_powerup extends powerup {
     execute(puck) {
         // reverse the velocity of the puck
         Phaser.Point.negative(this.player.puck.body.velocity, this.player.puck.body.velocity);
+
+        // if a goal is scored 'by' the flip, the player who activated the flip
+        // should get credit!
+        puck_history.push(this.player.name);
     }
 
     undo() {
